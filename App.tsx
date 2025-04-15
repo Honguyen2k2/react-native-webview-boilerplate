78d54024288febf40c788f1057e315b8795ac645
Honguyen.dev2k2 <Honguyen.dev2k2@gmail.com>
2025-04-15 22:26:34 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StatusBar, useColorScheme, View} from 'react-native';
+import WebView from 'react-native-webview';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <WebView source={{uri: 'https://reactnative.dev/'}} style={{flex: 1}} />
+    </View>
+  );
+}
+
+export default App;
